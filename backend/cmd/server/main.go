8c35d4d717ecba6c1d4f2fe413f474c4504cd97f
Lornajeruto <kiproplorna@gmail.com>
2024-06-05 18:56:31 +0300

    Update main.go

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,15 +1,42 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"whatsapp_clone/pkg/handlers"
-	"whatsapp_clone/pkg/websocket"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	socketio "github.com/googollee/go-socket.io"
 )
 
 func main() {
-	go websocket.HubInstance.Run()
-	http.HandleFunc("/", handlers.ServeHome)
-	http.HandleFunc("/ws", handlers.ServeWs)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	router := gin.Default()
+
+	server := socketio.NewServer(nil)
+
+	server.OnConnect("/", func(s socketio.Conn) error {
+		s.SetContext("")
+		fmt.Println("connected:", s.ID())
+		return nil
+	})
+
+	server.OnEvent("/", "message", func(s socketio.Conn, msg string) {
+		fmt.Println("message:", msg)
+		server.BroadcastToRoom("/", "chat", "message", msg)
+	})
+
+	server.OnDisconnect("/", func(c socketio.Conn, reason string) {
+		fmt.Println("closed", reason)
+	})
+
+	go server.Serve()
+	defer server.Close()
+
+	router.GET("/socket.io/*any", gin.WrapH(server))
+	router.POST("/socket.io/*any", gin.WrapH(server))
+
+	router.Static("/static", "./static")
+	router.GET("/", func(c *gin.Context) {
+		c.File("./static/index.html")
+	})
+
+	router.Run("0.0.0.0.8080")
 }
